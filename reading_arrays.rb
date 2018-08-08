91cc958e0831ba0d77c7b5431620640e6477b798
Abigail Slovick <abby.slovick@gmail.com>
2018-08-08 14:59:56 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -24,15 +24,13 @@ last_student_by_index
 
 def first_student_by_method
   STUDENT_NAMES.first
-  # Write a solution that returns the first student using the built in .first method
 end
 
 def last_student_by_method
   STUDENT_NAMES.last
-  # Write a solution that returns the first student using the built in .last method
 end
 
 def first_second_and_third_students
   STUDENT_NAMES[0..2]
-  # Write a solution that returns the first, second and third students
 end
+puts first_second_and_third_students
